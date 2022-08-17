9ee2fcf7ba98cae1dea77817411e4418f107c0df
teledima <teledima00@gmail.com>
2022-08-18 02:01:00 +0500

    Work on workflow

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const swaggerOptions = {
     apis: ['./src/routes/*.js'],
 }
 const apiSpec = swaggerDocument(swaggerOptions);
+const port = process.env.NODE_ENV === 'test' ? 8081 : 8080
 
 dotenv.config({
     path: `./.${process.env.NODE_ENV}.env`
@@ -59,7 +60,7 @@ app.get('/swagger.json', (_req, res) => res.json(apiSpec));
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(null, { swaggerOptions: { url: '/swagger.json' } }))
 app.use('/', routes)
 
-app.listen(8080, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log('listen to http://0.0.0.0:8080')
 })
 
